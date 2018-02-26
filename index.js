4110052a2680075d60078978b0333522d4d971e7
dkniazevych <dkniazevych@hiqinvest.nl>
2018-02-26 16:49:27 +0100

    update entry point

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    rulesDirectory: [
+        './dist/rules'
+    ],
+    rules: {
+        "tsr-detect-buffer-noassert": [true],
+        "tsr-detect-child-process": [true],
+        "tsr-detect-eval-with-expression": [true],
+        "tsr-detect-no-csrf-before-method-override": [true],
+        "tsr-detect-non-literal-buffer": [true],
+        "tsr-detect-non-literal-fs-filename": [true],
+        "tsr-detect-non-literal-regexp": [true],
+        "tsr-detect-non-literal-require": [true],
+        "tsr-detect-possible-timing-attacks": [true],
+        "tsr-detect-pseudo-random-bytes": [true],
+        "tsr-detect-unsafe-regexp": [true],
+        "tsr-disable-mustache-escape": [true]
+    }
+};
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    rulesDirectory: [
-        './rules'
-    ]
-};
